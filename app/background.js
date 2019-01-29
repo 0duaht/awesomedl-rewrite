4bfa93cb32e33e9fee32ba76c5f5798c83cd41ff
Tobi Oduah <tobi.oduah@andela.com>
2019-01-29 01:57:16 +0100

    Add support for seku.pw on Seriescr

--- app/background.js
+++ app/background.js
@@ -1,8 +1,8 @@
 (function() {
   'use strict';
 
-  chrome.webNavigation.onBeforeNavigate.addListener(function(navigation) {
-    if (navigation.url.indexOf("sh.st") == -1) return
+  var beforeNavigateCallback = function(navigation) {
+    if (!urlMatch(navigation.url)) return
 
     var fullUrl = navigation.url;
     var lastIndex = fullUrl.lastIndexOf("http")
@@ -10,5 +10,15 @@
       var destinationUrl = fullUrl.substring(lastIndex)
       chrome.tabs.update(navigation.tabId, {url: destinationUrl})
     }
-  });
+  }
+
+  var urlMatch = function(url) {
+    const urls = ['sh.st', 'seku.pw']
+    for (var i = 0; i < urls.length; i++) {
+      if (url.indexOf(urls[i]) != -1) return true;
+    }
+
+    return false;
+  }
+  chrome.webNavigation.onBeforeNavigate.addListener(beforeNavigateCallback);
 })();
